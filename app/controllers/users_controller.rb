5a49fd44a0153366575b469ee144129e4dc534fb
Raghu Betina <rbetina@gmail.com>
2012-08-27 09:30:10 -0500

    Proved we got access token.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class UsersController < ApplicationController
   before_filter :require_login, :only => [:show, :edit, :update, :delete]
   
@@ -22,6 +24,9 @@ class UsersController < ApplicationController
   def authorize
     @code = params[:code]
     
+    uri = "https://graph.facebook.com/oauth/access_token?client_id=434825803222733&redirect_uri=http://localhost:3000/auth&client_secret=ac5251e1281670a5edec924f33a61cc7&code=#{@code}"
+    
+    @response = open(uri).read
   end
 
   def index
